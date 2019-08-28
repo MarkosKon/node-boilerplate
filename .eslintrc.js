e4440b5933a35ed4f1012ba50494d270c726aa5c
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-08-28 11:57:51 +0300

    Added tests with react-scripts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,16 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
+    "jest/globals": true
   },
   extends: [
     "airbnb-base",
     "plugin:prettier/recommended",
-    "plugin:node/recommended"
+    "plugin:node/recommended",
+    "plugin:jest/recommended"
   ],
+  plugins: ["jest"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
